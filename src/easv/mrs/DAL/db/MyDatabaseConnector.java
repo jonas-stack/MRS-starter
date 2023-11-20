84468857519222685d2946d4f8c58c2d7bc5d2c5
Jonas Speedtsberg <jonasspeedtsberg99@gmail.com>
2023-11-20 14:24:46 +0100

    Update MyDatabaseConnector.java

--- src/easv/mrs/DAL/db/MyDatabaseConnector.java
+++ src/easv/mrs/DAL/db/MyDatabaseConnector.java
@@ -3,18 +3,31 @@ package easv.mrs.DAL.db;
 
 public class MyDatabaseConnector {
 
+<<<<<<< Updated upstream
 
     //Class will easv.mrs.be included when we start working on DATABASES
     /*
+=======
+    private static final String PROP_FILE = "config/config.settings";
+    //Class will easv.mrs.be included when we start working on DATABASES
+
+>>>>>>> Stashed changes
     private SQLServerDataSource dataSource;
 
     public MyDatabaseConnector()
     {
         dataSource = new SQLServerDataSource();
+<<<<<<< Updated upstream
         dataSource.setServerName("");
         dataSource.setDatabaseName("");
         dataSource.setUser("");
         dataSource.setPassword("");
+=======
+        dataSource.setServerName(databaseProperties.getProperty("Server"));
+        dataSource.setDatabaseName(databaseProperties.getProperty("Database"));
+        dataSource.setUser(databaseProperties.getProperty("User"));
+        dataSource.setPassword(databaseProperties.getProperty("Password"));
+>>>>>>> Stashed changes
         dataSource.setPortNumber(1433);
     }
 
@@ -33,6 +46,9 @@ public class MyDatabaseConnector {
 
         } //Connection gets closed here
     }
+<<<<<<< Updated upstream
     */
 
+=======
+>>>>>>> Stashed changes
 }
